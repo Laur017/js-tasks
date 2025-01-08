7dbedc394e6c128c835998668a181d7b5ce03e48
Popa Laurentiu <lpopa@griddynamics.com>
2025-01-08 15:04:09 +0200

    3rd problem solved

--- js-practice/tasks/3_is-boolean/is-boolean.js
+++ js-practice/tasks/3_is-boolean/is-boolean.js
@@ -1,6 +1,3 @@
 const isBoolean = (param) => typeof param === 'boolean';
 
-console.log(isBoolean(true));
-console.log(isBoolean(false));
-console.log(isBoolean([1, 2, 3]));
-console.log(isBoolean([].slice));
+export { isBoolean };
