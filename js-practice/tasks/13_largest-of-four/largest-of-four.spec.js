9437e487d1bc143c9228ea9d8f6dec6084e4b437
Popa Laurentiu <lpopa@griddynamics.com>
2025-01-09 14:58:21 +0200

    13th problem solved

--- js-practice/tasks/13_largest-of-four/largest-of-four.spec.js
+++ js-practice/tasks/13_largest-of-four/largest-of-four.spec.js
@@ -1,6 +1,6 @@
-// import { largestOfFour } from './largest-of-four';
+import { largestOfFour } from './largest-of-four';
 
-describe.skip('largestOfFour', () => {
+describe('largestOfFour', () => {
   it('largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]) should return an array', () => {
     expect(
       Array.isArray(
@@ -8,7 +8,7 @@ describe.skip('largestOfFour', () => {
           [4, 5, 1, 3],
           [13, 27, 18, 26],
           [32, 35, 37, 39],
-          [1000, 1001, 857, 1],
+          [1000, 1001, 857, 1]
         ])
       )
     ).toBe(true);
@@ -20,7 +20,7 @@ describe.skip('largestOfFour', () => {
         [13, 27, 18, 26],
         [4, 5, 1, 3],
         [32, 35, 37, 39],
-        [1000, 1001, 857, 1],
+        [1000, 1001, 857, 1]
       ])
     ).toEqual([27, 5, 39, 1001]);
   });
@@ -31,7 +31,7 @@ describe.skip('largestOfFour', () => {
         [4, 9, 1, 3],
         [13, 35, 18, 26],
         [32, 35, 97, 39],
-        [1000000, 1001, 857, 1],
+        [1000000, 1001, 857, 1]
       ])
     ).toEqual([9, 35, 97, 1000000]);
   });
