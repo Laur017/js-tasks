bc8f4214e610ccc89becdc3af8f79597f545e9e0
Popa Laurentiu <lpopa@griddynamics.com>
2025-01-15 15:48:29 +0200

    18th problem updated solution

--- js-practice/tasks/18_humanize-format/humanize-format.js
+++ js-practice/tasks/18_humanize-format/humanize-format.js
@@ -2,12 +2,10 @@ export function humanizeFormat() {
   const ending = ['th', 'st', 'nd', 'rd'];
   const number = arguments[0];
 
-  if (arguments.length > 0) {
-    const end = number % 10;
-    if ((number > 3 && number < 21) || end > 3) {
-      return number + ending[0];
-    }
-    return number + ending[end];
+  if (!arguments.length) return '';
+  const end = number % 10;
+  if ((number > 3 && number < 21) || end > 3) {
+    return number + ending[0];
   }
-  return '';
+  return number + ending[end];
 }
