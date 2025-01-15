3d98406f4fb8a7caffd6ba217e69d3cf9a433e40
Popa Laurentiu <lpopa@griddynamics.com>
2025-01-15 16:07:17 +0200

    25th problem implemented with dictionary

--- js-practice/tasks/25_rot13-encoder/rot13-encoder.js
+++ js-practice/tasks/25_rot13-encoder/rot13-encoder.js
@@ -1,11 +1,12 @@
-export const rot13Encoder = (str) =>
-  [...str]
-    .map((element) => {
-      const code = element.charCodeAt();
-      if (code > 64 && code < 91) {
-        const finalCode = code + 13;
-        return String.fromCharCode(finalCode > 90 ? finalCode - 26 : finalCode);
-      }
-      return element;
-    })
-    .join('');
+export const rot13Encoder = (str) => {
+  let rot13_Dictionary = {};
+  for (let i = 65; i < 91; i++) {
+    const finalCode = i + 13;
+    rot13_Dictionary[String.fromCharCode(i)] = String.fromCharCode(finalCode > 90 ? finalCode - 26 : finalCode);
+    rot13_Dictionary[String.fromCharCode(i).toLowerCase()] = String.fromCharCode(
+      finalCode > 90 ? finalCode - 26 : finalCode
+    ).toLowerCase();
+  }
+
+  return [...str].map((i) => rot13_Dictionary[i] ?? i).join('');
+};
