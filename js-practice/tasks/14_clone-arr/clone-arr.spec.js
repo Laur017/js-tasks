397e37ed4651f52409a8ac7887ab0e3b08ff87d5
Popa Laurentiu <lpopa@griddynamics.com>
2025-01-15 15:42:07 +0200

    updated tests

--- js-practice/tasks/14_clone-arr/clone-arr.spec.js
+++ js-practice/tasks/14_clone-arr/clone-arr.spec.js
@@ -13,4 +13,20 @@ describe('cloneArr', () => {
 
     expect(original).toHaveLength(5);
   });
+
+  it('should not be a ref to the original array', () => {
+    const original = [];
+    const copy = cloneArr(original);
+    expect(original === copy).toBe(false);
+  });
+
+  it('should return a deep clone of the array', () => {
+    const a = { a: 1 };
+    const b = { a: 1 };
+    const arr = [a, b];
+    const newArr = cloneArr(arr);
+    arr[0].a = 2;
+    expect(arr).toStrictEqual([{ a: 2 }, { a: 1 }]);
+    expect(newArr).toStrictEqual([{ a: 1 }, { a: 1 }]);
+  });
 });
