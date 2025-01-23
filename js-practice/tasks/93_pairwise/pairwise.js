cb65ea83dcacabf998321b1cac0802c7b8941197
Popa Laurentiu <lpopa@griddynamics.com>
2025-01-23 15:11:02 +0200

    93rd problem 1/2 solved

---
+++ js-practice/tasks/93_pairwise/pairwise.js
@@ -0,0 +1,14 @@
+export const pairwise = (arr, arg) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === arg) {
+        sum += i + j;
+        arr[i] = arr[j] = null;
+        break;
+      }
+    }
+  }
+
+  return sum;
+};
