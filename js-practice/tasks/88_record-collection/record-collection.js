0ae36a64f1ef84b9cf1e3a382ce3326ae1800ee2
Popa Laurentiu <lpopa@griddynamics.com>
2025-02-04 11:12:18 +0200

    88th problem solved

--- js-practice/tasks/88_record-collection/record-collection.js
+++ js-practice/tasks/88_record-collection/record-collection.js
@@ -1,9 +1,36 @@
 export const updateRecords = (id, prop, value) => {
-  return {
-    ...collection,
-    [id]: {
-      ...collection[id],
-      [prop]: prop === 'tracks' ? collection[id][prop].push(value) : value
+  let collection = {
+    2548: {
+      album: 'Slippery When Wet',
+      artist: 'Bon Jovi',
+      tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+      album: '1999',
+      artist: 'Prince',
+      tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+      artist: 'Robert Palmer',
+      tracks: []
+    },
+    5439: {
+      album: 'ABBA Gold'
     }
   };
+
+  if (value === '') {
+    delete collection[id][prop];
+  } else {
+    if (prop === 'tracks') {
+      if (!collection[id].tracks) {
+        collection[id].tracks = [];
+      }
+      collection[id].tracks.push(value);
+    } else {
+      collection[id][prop] = value;
+    }
+  }
+
+  return collection;
 };
