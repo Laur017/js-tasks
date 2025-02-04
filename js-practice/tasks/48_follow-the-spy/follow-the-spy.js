ecabf5af743d9eb98ebb845ea1ab7f4ca9999687
Popa Laurentiu <lpopa@griddynamics.com>
2025-02-04 09:56:37 +0200

    48th problem solved

--- js-practice/tasks/48_follow-the-spy/follow-the-spy.js
+++ js-practice/tasks/48_follow-the-spy/follow-the-spy.js
@@ -1,45 +1,29 @@
 export const followTheSpy = (arr) => {
   const findFirstLastCountry = () => {
-    let first = '';
-    let last = '';
     let s1 = [];
     let s2 = [];
+
     for (let i = 0; i < arr.length; i++) {
       s1.push(arr[i][0]);
       s2.push(arr[i][1]);
     }
-    for (let i in s1) {
-      if (!s2.includes(s1[i])) {
-        first = s1[i];
-      }
-      if (!s1.includes(s2[i])) {
-        last = s2[i];
-      }
-    }
-    return [first, last];
-  };
-  let [first, last] = findFirstLastCountry();
-  let answ = [arr[0]];
-  let q = 0;
 
-  console.log(first, last);
-  console.log(answ[0]);
-  for (let i = 0; i < arr.length; i++) {
-    if (answ[q][1] === arr[i][0]) {
-      answ.push(arr[i]);
-      q++;
-      i = 0;
-    }
-    if (answ[q][1] === last) {
-      q++;
-      //todo
+    return [s1.find((city) => !s2.includes(city)), s2.find((city) => !s1.includes(city))];
+  };
+
+  let [first, last] = findFirstLastCountry();
+  let route = [first];
+  let currentCity = first;
+
+  while (route.length < arr.length + 1) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i][0] === currentCity) {
+        route.push(arr[i][1]);
+        currentCity = arr[i][1];
+        break;
+      }
     }
   }
 
-  answ = answ
-    .flat()
-    .filter((_, i) => i % 2 === 1)
-    .slice(0, answ.length - 1)
-    .join(', ');
-  return `${first}, ${answ}, ${last}`;
+  return route.join(', ');
 };
